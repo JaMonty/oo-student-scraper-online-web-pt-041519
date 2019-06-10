c58127a25472b0ee2fa537de9c841d579663cfaf
Jason  Montes <jasone.montes@gmail.com>
2019-06-10 03:15:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,25 +22,25 @@ class Scraper
 
 
   def self.scrape_profile_page(profile_url)
-    student = {}
+    stud = {}
     profile_page = Nokogiri::HTML(open(profile_url))
     links = profile_page.css(".social-icon-container").children.css("a").map { |el| el.attribute('href').value}
     links.each do |link|
       if link.include?("linkedin")
-        student[:linkedin] = link
+        stud[:linkedin] = link
       elsif link.include?("github")
-        student[:github] = link
+        stud[:github] = link
       elsif link.include?("twitter")
-        student[:twitter] = link
+        stud[:twitter] = link
       else
-        student[:blog] = link
+        stud[:blog] = link
       end
     end
 
-    student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
-    student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
+    stud[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
+    stud[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
 
-    student
+    stud
   end
 
 end
